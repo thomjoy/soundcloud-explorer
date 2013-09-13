36ee6e8975f59653be26bb88317383741cd9e4bf
Thom Joy <thomjoy1984@gmail.com>
2013-09-13 12:53:28 +1000

    3 col layout

--- v/favouriteitem.js
+++ v/favouriteitem.js
@@ -21,7 +21,7 @@ define([
 
     render: function() {
       var templateData = {
-        image: this.model.get('artwork_url') ? this.model.get('artwork_url') : this.model.attributes.user.avatar_url,
+        image: this.model.get('artwork_url') ? this.model.get('artwork_url') : this.model.get('user').avatar_url,
         tags: this.model.get('tag_list').split(' '),
         date: moment(new Date(this.model.get('created_at'))).format('ddd Do MMM YYYY'),
         duration:  Math.ceil((this.model.get('duration') / 1000) / 60),
