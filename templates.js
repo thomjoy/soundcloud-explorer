8e0c40acdbf6fc4039b077ef10fb18fa12d28cac
Thom Joy <thomjoy1984@gmail.com>
2013-09-19 18:09:04 +1000

    deletes

--- templates.js
+++
@@ -1,7 +0,0 @@
-define(function(require) {
-  return {
-    favourites: require('text!t/favourites.html'),
-    favouriteItem: require('text!t/favouritesitem.html'),
-    user: require('text!t/user.html'),
-  };
-});
